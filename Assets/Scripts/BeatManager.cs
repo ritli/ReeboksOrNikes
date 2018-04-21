789941301d08e7fc46f3cfd12dd26073ea88e47e
ritli <agnar_oskarsson@hotmail.com>
2018-04-21 16:05:32 +0200

    Work on beat manager

--- Assets/Scripts/BeatManager.cs
+++ Assets/Scripts/BeatManager.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BeatManager : MonoBehaviour {
 
-	// Use this for initialization
+    [FMODUnity.EventRef]
+    public string MusicEvent;
+
+    FMODUnity.StudioEventEmitter emitter;
+
+    float currentBpm;
+    float beatTime;
+    float currentBeatTime;
+    int currentBeat = 0;
+
+    public Slider slider;
+    public Image sliderImage;
+
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+        sliderImage = slider.GetComponent<Image>();
+
+        emitter = GetComponent<FMODUnity.StudioEventEmitter>();
+        emitter.Event = MusicEvent;
+        emitter.Play();
+        SetBPM(109);
+    }
+
+    void SetBPM(float bpm)
+    {
+        beatTime = 60 / bpm;
+        currentBpm = bpm;
+    }
+
 	void Update () {
-		
+        currentBeatTime += Time.deltaTime / beatTime;
+        currentBeatTime = currentBeatTime % 1;
+
+        slider.value = currentBeatTime;
+
 	}
+
+    IEnumerator OnBeat()
+    {
+
+    }
 }
