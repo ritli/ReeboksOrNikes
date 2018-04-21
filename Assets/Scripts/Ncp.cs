1bb759b86087e9d309f2e8982fb717ae83d5c30b
warhammster40k <olof.hedstrom@gmail.com>
2018-04-22 01:17:31 +0200

    Dialog system och flera tiles också kod till NCP i allmenhet

---
+++ Assets/Scripts/Ncp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ncp : MonoBehaviour {
+
+    public Dialoge dialoge;
+    private bool Started = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("space"))
+        {
+            if (!Started)
+            {
+                //Debug.Log("Swag Cat Name   " + dialoge.name);
+                FindObjectOfType<DialogeManager>().startDialoge(dialoge);
+                Started = true;
+            }
+            else
+            {
+                FindObjectOfType<DialogeManager>().DisplayNextDialoge();
+            }
+        }
+    }
+
+    
+}
