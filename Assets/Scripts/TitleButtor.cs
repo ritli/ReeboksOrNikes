7bbacbd93d9e8a2d35b8a70e68c25bea556d664c
warhammster40k <olof.hedstrom@gmail.com>
2018-04-22 23:44:51 +0200

    made the lats cates, titelscreen is done

---
+++ Assets/Scripts/TitleButtor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleButtor : MonoBehaviour {
+
+    public Canvas CreditCanvas;
+
+
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void changeScene()
+    {
+        //SceneManager.LoadScene("SceneName", LoadSceneMode.single);
+        Debug.Log("Change scene");
+    }
+
+    public void displayCredits()
+    {
+        CreditCanvas.gameObject.SetActive(true);
+    }
+
+    public void CloseCanvas()
+    {
+        CreditCanvas.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/betMatshScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class betMatshScript : MonoBehaviour {
+
+    Animator animator;
+
+    void Start () {
+        animator = GetComponent<Animator>();
+
+        animator.SetFloat("SpeedMultiplier", BeatManager.GetCurrentBPM / 60);
+    }
+	
+}
