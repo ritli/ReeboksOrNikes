2c93c51552ad68d639ef98900ce97f4071afd57a
Nerfertius <nerfertius@gmail.com>
2018-04-22 17:35:40 +0200

    Respawn done

---
+++ Assets/Scripts/FailState.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FailState : MonoBehaviour
+{
+	public Transform respawn;
+	public GameObject fadeScreen;
+
+	private int losses;
+	private float lerpValue;
+	private bool hitable;
+	private bool fadeOut;
+	private bool fadeIn;
+	private Color fadeColor;
+	private Color newColor;
+
+	private void Start()
+	{
+		BeatManager.onBeat += OnBeat;
+		fadeColor = fadeScreen.GetComponent<Image>().color;
+		newColor = fadeColor;
+	}
+
+	void OnBeat(int count)
+	{
+		if (fadeOut && (count == 0 || count == 2))
+		{
+			newColor.a += 0.25f;
+			fadeScreen.GetComponent<Image>().color = newColor;
+			if (newColor.a > 1)
+			{
+				newColor.a = 1f;
+				fadeOut = false;
+				fadeIn = true;
+			}
+		}
+
+		if (fadeIn && (count == 0 || count == 2))
+		{
+			newColor.a -= 0.25f;
+			fadeScreen.GetComponent<Image>().color = newColor;
+			if (newColor.a < 0)
+			{
+				newColor.a = 0f;
+				fadeIn = false;
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Enemy") && hitable)
+		{
+			fadeOut = true;
+			hitable = false;
+		}
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Y))
+		{
+			fadeOut = true;
+			print("Testing respawn");
+		}
+	}
+}
